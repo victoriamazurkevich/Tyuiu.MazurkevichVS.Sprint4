eec4ec4027ffbccac3df9dd1b81545f5358ec948
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-11-13 22:22:28 +0500

    Task0

--- Tyuiu.MazurkevichVS.Sprint4.Task0.V26.Lib/DataService.cs
+++ Tyuiu.MazurkevichVS.Sprint4.Task0.V26.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.MazurkevichVS.Sprint4.Task0.V26.Lib
             int s = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] % 2 == 0)
+                if (array[i] % 2 != 0)
                     s += array[i];
             } 
             return s;
