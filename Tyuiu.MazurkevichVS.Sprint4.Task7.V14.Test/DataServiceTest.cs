38aec49742a0d80905b31aabe7dd332cfb6b42f3
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-11-14 22:27:11 +0500

    Task7

---
+++ Tyuiu.MazurkevichVS.Sprint4.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MazurkevichVS.Sprint4.Task7.V14.Lib;
+namespace Tyuiu.MazurkevichVS.Sprint4.Task7.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int n = 3;
+            int m = 4;
+            string value = "458712659137";
+            int res = ds.Calculate(n, m, value);
+            int expected = 38;
+            Assert.AreEqual(expected, res);
+
+        }
+    }
+}
