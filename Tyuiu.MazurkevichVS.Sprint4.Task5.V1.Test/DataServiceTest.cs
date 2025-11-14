c8d0aa4ea2b33915bbf1c8d340fc9b62ae040b2b
Виктория Мазуркевич <victoria090707maz@gmail.com>
2025-11-14 21:43:36 +0500

    Task5

---
+++ Tyuiu.MazurkevichVS.Sprint4.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.MazurkevichVS.Sprint4.Task5.V1.Lib;
+namespace Tyuiu.MazurkevichVS.Sprint4.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] array = new int[,]{{ 6, 6, 5, 3, 3 },
+                                      { 5, -7, 4, 6, 4 },
+                                      { 1, 2, -4, 1, 5 },
+                                      { 1, 7, 2, -5, 7 },
+                                      { 4, 2, 6, 5, 6 }};
+            int[,] expected = new int[,]{{ 1, 1, 1, 1, 1 },
+                                      { 1, -7, 1, 1, 1 },
+                                      { 1, 1, -4, 1, 1 },
+                                      { 1, 1, 1, -5, 1 },
+                                      { 1, 1, 1, 1, 1 }};
+            int[,] result = ds.Calculate(array);
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.MazurkevichVS.Sprint4.Task5.V1/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MazurkevichVS.Sprint4.Task5.V1.Lib;
+DataService ds = new DataService();
+Random rnd = new Random();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Исходный массив:");
+Console.WriteLine("Введите кол-во строк и столбцов:");
+int rows = Convert.ToInt32(Console.ReadLine());
+int length = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, length];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < length; j++)
+    {
+        array[i, j] = rnd.Next(-9, 8);
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Ваш массив");
+for (int i = 0; i < length; i++)
+{
+    for (int j = 0; j < rows; j++)
+    {
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+int[,] res = ds.Calculate(array);
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Ваш массив: ");
+for (int i = 0; i < length; i++)
+{
+    for (int j = 0; j < rows; j++)
+    {
+        Console.Write(res[i, j] + "\t");
+    }
+
+}
+Console.WriteLine(res);
+Console.ReadKey();
